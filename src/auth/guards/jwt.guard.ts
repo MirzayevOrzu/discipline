c9bdca3788e61fd4107a9ebc190b1174ad9df432
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-04-19 12:33:16 +0500

    Fix jwt auth guard

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
