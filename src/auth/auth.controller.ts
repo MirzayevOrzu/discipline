6680170cb5aae2a26cabbf7278bf06de1786e1e9
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-04-19 12:29:27 +0500

    Example protected route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
+import { CurrentUser } from '../common/decorators/CurrentUser';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { JwtAuthGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,11 @@ export class AuthController {
   @Post('login')
   login(@Body() loginData: CreateAuthDto) {
     return this.authService.login(loginData);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  example(@CurrentUser() user) {
+    return user;
   }
 }
